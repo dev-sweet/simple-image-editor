25b42ee2b622e867ecce32576a8c85fe79a5fc0d
dev-sweet <sweetali0520@gmail.com>
2023-01-30 00:58:49 +0600

    filters functionality added

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,30 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.scss";
 import { AiOutlineRotateLeft, AiOutlineRotateRight } from "react-icons/ai";
 import { CgEditFlipH, CgEditFlipV } from "react-icons/cg";
 import { ImUndo, ImRedo } from "react-icons/im";
 import { RiImageAddLine } from "react-icons/ri";
 const Main = () => {
+  // states
+  // image states (all image property here)
+  const [image, setImage] = useState({
+    url: "",
+    Brightness: 100,
+    Grayscale: 0,
+    Sepia: 0,
+    Saturate: 100,
+    Contrast: 100,
+    HueRotate: 0,
+    Rotate: 0,
+    Vertical: 1,
+    Horizontal: 1,
+  });
+  // distracture all about the image
+  const {
+    url,
+    Brightness,
+    Grayscale,
+    Sepia,
+    Saturate,
+    Contrast,
+    HueRotate,
+    Rotate,
+    Vertical,
+    Horizontal,
+  } = image;
+
+  // changable imaage propery and their value
+  const [property, setProperty] = useState({
+    name: "Brightness",
+    maxValue: 200,
+  });
+
+  // state for range
+  const [range, setRange] = useState(image.Brightness);
+
+  // filters property of image
   const filters = [
     {
-      name: "brigthness",
+      name: "Brightness",
+      maxValue: 200,
     },
     {
-      name: "grayscale",
+      name: "Grayscale",
+      maxValue: 200,
     },
     {
-      name: "sepia",
+      name: "Sepia",
+      maxValue: 200,
     },
     {
-      name: "saturate",
+      name: "Saturate",
+      maxValue: 200,
     },
     {
-      name: "contrast",
+      name: "Contrast",
+      maxValue: 200,
     },
     {
-      name: "rotate",
+      name: "HueRotate",
+      maxValue: 200,
     },
   ];
+
+  // when user select an image then triger this handle change function
+  const handleChange = (e) => {
+    if (e.target.files.length) {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = () => {
+        setImage({ ...image, url: reader.result });
+      };
+    }
+  };
+
+  // when clik a button(want to change property like as brightness,contrast,etx)
+  const handleClick = (prop) => {
+    setProperty(prop);
+    setRange(image[prop.name]);
+  };
+
+  const handleRange = (e) => {
+    setRange(e.target.value);
+    setImage({ ...image, [e.target.name]: e.target.value });
+  };
+
+  // const changValueOfPropery = (value) => {};
   return (
     <div className='main'>
       <div className='editor-board'>
@@ -32,51 +100,82 @@ const Main = () => {
           <h1 className='title'>---image Editor---</h1>
         </div>
         <div className='editor-body'>
-          <div className='sidebar'>
-            <div className='filters-body'>
-              <span>Filters</span>
-              <div className='filters'>
-                {filters.map((filter, i) => (
-                  <button className='edit-btn' key={i}>
-                    {filter.name}
+          <div className='sidebar-container'>
+            <div className='sidebar'>
+              <div className='filters-body'>
+                <span>Filters</span>
+                <div className='filters'>
+                  {filters.map((filter, i) => (
+                    <button
+                      onClick={() => handleClick(filter)}
+                      className={`edit-btn ${
+                        property.name === filter.name && "active"
+                      }`}
+                      key={i}
+                      disabled={!url}
+                    >
+                      {filter.name}
+                    </button>
+                  ))}
+                </div>
+              </div>
+              <div className='filter-slider'>
+                <div className='label-bar'>
+                  <label htmlFor='rotate'>{property.name}</label>
+                  <span>{`${range}%`}</span>
+                </div>
+                <input
+                  name={property.name}
+                  value={range}
+                  onChange={handleRange}
+                  max={property.maxValue}
+                  disabled={!url}
+                  type='range'
+                />
+              </div>
+              <div className='rotate-and-flip'>
+                <label htmlFor=''>Rotate & Flip</label>
+                <div className='icons'>
+                  <button>
+                    <AiOutlineRotateLeft />
                   </button>
-                ))}
+                  <button>
+                    <AiOutlineRotateRight />
+                  </button>
+                  <button>
+                    <CgEditFlipH />
+                  </button>
+                  <button>
+                    <CgEditFlipV />
+                  </button>
+                </div>
               </div>
             </div>
-            <div className='filter-slider'>
-              <div className='label-bar'>
-                <label htmlFor='rotate'>Rotate</label>
-                <span>100%</span>
-              </div>
-              <input name='rotate' type='range' />
-            </div>
-            <div className='rotate-and-flip'>
-              <label htmlFor=''>Rotate & Flip</label>
-              <div className='icons'>
-                <button>
-                  <AiOutlineRotateLeft />
-                </button>
-                <button>
-                  <AiOutlineRotateRight />
-                </button>
-                <button>
-                  <CgEditFlipH />
-                </button>
-                <button>
-                  <CgEditFlipV />
-                </button>
-              </div>
+            <div className='save-and-reset'>
+              <button className='reset'>Reset</button>
+              <button className='save'>Save</button>
             </div>
           </div>
           <div className='select-image-area'>
             <div className='image'>
-              <label htmlFor='choose'>
-                <span className='select-image'>
-                  <RiImageAddLine />
-                </span>
+              {image?.url ? (
+                <img
+                  style={{
+                    filter: `brightness(${Brightness}%) sepia(${Sepia}%) grayscale(${Grayscale}%) contrast(${Contrast}%) saturate(${Saturate}%) hue-rotate(${HueRotate}deg)`,
+                    transform: `rotate(${Rotate}deg) scale(${Vertical}) scale(${Horizontal})`,
+                  }}
+                  src={url}
+                  alt=''
+                />
+              ) : (
+                <label htmlFor='choose'>
+                  <span className='select-image'>
+                    <RiImageAddLine />
+                  </span>
 
-                <span>Choose image</span>
-              </label>
+                  <span>Choose image</span>
+                </label>
+              )}
             </div>
             <div className='btns'>
               <button className='undo'>
@@ -91,7 +190,7 @@ const Main = () => {
                 Choose Image
               </label>
             </div>
-            <input type='file' id='choose' />
+            <input type='file' onChange={handleChange} id='choose' />
           </div>
         </div>
       </div>
